e9ab679645f91a8275ad7cad587308177eaa16d9
giorgi <giorgichqareuli712@gmail.com>
2024-11-05 15:44:58 +0400

    first commit

---
+++ loops/src/Main.java
@@ -0,0 +1,82 @@
+public class Main {
+    public static void main(String[] args) {
+        // ეს არის while ციკლი
+//        int counter = 1;
+//        while (counter <= 5) {
+//            System.out.println(counter);
+//            counter ++;
+//        }
+
+
+
+
+//        // ეს არის do-while ციკლი
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i++;
+//        }
+//        while (i < 5);
+
+
+
+
+        // ეს არის for ციკლი
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println(i);
+//            // i ხელმისაწვდომია მხოლოდ ციკლის შიგნით
+//        }
+
+
+
+
+        // break ოპერატორი
+//        int count = 1;
+//        while (true) {
+//            System.out.println("Hello");
+//            count ++;
+//
+//            if (count > 5) {
+//                break;
+//            }
+//        }
+//
+//
+//        for (int i = 1; true; i ++) {
+//            System.out.println("Hello from for loop");
+//            if (i > 10);
+//            System.out.println("Hoop is over");
+//            break;
+//        }
+
+
+
+
+        // continue ოპერატორი
+//        int num = 0;
+//        while (num < 10) {
+//            num++;
+//
+//            if (num % 2 == 0) {
+//                continue;
+//            }
+//            System.out.println(num);
+//        }
+
+
+
+
+        // ჩადგმული ციკლები
+//        for (int i = 1; i <= 3; i++) {
+//            for (int j = 1; j <= 3; j++) {
+//                System.out.println("(" + i + "," + j + ") ");
+//            }
+//            System.out.println();
+//        }
+
+
+
+
+
+    }
+}
